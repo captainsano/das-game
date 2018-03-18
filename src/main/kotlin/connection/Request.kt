94340b7dc957a55154339dc2e3929e8fb050c75b
Santhos Baala RS <rssanthosbaala@gmail.com>
2018-03-18 19:08:25 +0100

    Begin afresh

--- src/main/kotlin/connection/Request.kt
+++
@@ -1,8 +0,0 @@
-package connection
-
-import common.BattleUnit
-import java.io.Serializable
-
-class Request(val row: Int,
-              val col: Int,
-              val state: BattleUnit) : Serializable
